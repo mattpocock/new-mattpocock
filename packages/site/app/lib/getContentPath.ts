981b9b61f216a32cd3812b5d3da2d86d265b06da
Matt Pocock <mattpocockvoice@gmail.com>
2022-08-02 14:40:12 +0100

    Moonshot

--- packages/site/app/lib/getContentPath.ts
+++ packages/site/app/lib/getContentPath.ts
@@ -2,6 +2,4 @@ import * as path from 'path';
 
 export const getContentPath = (): string => {
   return path.resolve(process.cwd(), './content');
-
-  // return path.resolve(process.cwd(), '../../../content');
 };
